7f9c7a84b1eb27b2d516b203f4168f59fbe1b521
Jessica Melin <ataeraxiaDev@gmail.com>
2017-05-30 08:58:24 -0400

    Styling the Situation Activity
    Sit Activity now has a much more material design. I'm also working on
    adding new activities to explain how to use the app.

--- app/src/main/java/com/example/android/logthoughts/SitActivity.java
+++ app/src/main/java/com/example/android/logthoughts/SitActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SitActivity extends AppCompatActivity {
 
@@ -21,12 +22,23 @@ public class SitActivity extends AppCompatActivity {
             // The code in this method will be executed when the next button is clicked on.
             @Override
             public void onClick(View view) {
-                EditText getSitRecord = (EditText) findViewById(R.id.sit);
+                EditText getSitRecord = (EditText) findViewById(R.id.sit_edit);
                 String sitRecord = getSitRecord.getText().toString();
 
                 Intent nextIntent = new Intent(SitActivity.this, MoodsActivity.class);
                 nextIntent = nextIntent.putExtra("com.example.android.logthoughts.sitRecord", sitRecord);
                 startActivity(nextIntent);
+            }
+        });
+
+        TextView sitHelpText = (TextView) findViewById(R.id.help_text);
+
+        sitHelpText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent sitHelpIntent = new Intent(SitActivity.this, SitHelpActivity.class);
+                startActivity(sitHelpIntent);
             }
         });
     }
---
+++ app/src/main/java/com/example/android/logthoughts/SitHelpActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.logthoughts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SitHelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sit_help);
+    }
+}
